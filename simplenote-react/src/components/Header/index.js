e046d628b34c1b31a5e59e723cb8c0b42b29b013
hmoon630 <s19017@gsm.hs.kr>
2019-10-07 17:30:10 +0900

    add memo save

--- simplenote-react/src/components/Header/index.js
+++ simplenote-react/src/components/Header/index.js
@@ -4,7 +4,7 @@ import logo from '../../logo.png';
 
 class Header extends React.Component {
     render () {
-        const { onAddNote, onDeleteNote } = this.props;
+        const { onAddNote, onDeleteNote, onSaveNote } = this.props;
         return (
             <div className="header">
                 <div className="title">
@@ -14,6 +14,7 @@ class Header extends React.Component {
                     <span>현문's 심플노트</span>
                 </div>
                 <div className="buttons">
+                    <button onClick={onSaveNote}>저장</button>
                     <button onClick={onAddNote}>추가</button>
                     <button onClick={onDeleteNote}>삭제</button>
                 </div>
