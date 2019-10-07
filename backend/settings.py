4d6e685a76cec5e156ac1ec271f486c4305c4d0d
hmoon630 <s19017@gsm.hs.kr>
2019-10-07 17:27:31 +0900

    update db config

--- backend/settings.py
+++ backend/settings.py
@@ -94,8 +94,15 @@ CORS_ORIGIN_WHITELIST = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'admin',
+        'PASSWORD': 'moon!',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
