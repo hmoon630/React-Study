ea4ec0c960d87b361ffd133650841ccd9f4b52c2
hmoon630 <s19017@gsm.hs.kr>
2019-10-07 17:29:00 +0900

    memo

--- memo/urls.py
+++ memo/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.MemoList.as_view()),
-    path('<int:pk>/', views.MemoDetail.as_view()),
+    path('add/<str:pk>', views.MemoList.as_view()),
+    path('delete/<str:pk>',views.MemoDetail.as_view()),
+    path('save/<str:pk>', views.MemoDetail.as_view()),
+    path('<str:pk>/', views.MemoDetail.as_view()),
 ]
--- memo/views.py
+++ memo/views.py
@@ -1,14 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.response import Response
 
 from .models import Memo
 from .serializers import MemoSerializer
 
 # Create your views here.
-class MemoList(generics.ListCreateAPIView):
+ 
+class MemoSave(generics.UpdateAPIView):
+    serializer_class = MemoSerializer
+    def put(self, request):
+        memo = request.data
+        serializer = MemoSerializer(data=memo)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({"id": Memo.objects.last().id})
+
+class MemoDelete(generics.DestroyAPIView):
+    serializer_class = MemoSerializer
+    def delete(self, request, *args, **kwargs):
+        pass
+
+
+class MemoList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
     queryset = Memo.objects.all()
     serializer_class = MemoSerializer
 
-class MemoDetail(generics.RetrieveUpdateDestroyAPIView):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class MemoDetail(mixins.UpdateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+
     queryset = Memo.objects.all()
     serializer_class = MemoSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
